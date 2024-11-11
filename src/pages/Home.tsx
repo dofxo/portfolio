40a9a9b150ab4a6286501c882e0181d2f41c941e
Mohammad Kargar <kargat504@gmail.com>
2024-11-12 01:06:35 +0330

    Refactor: change home page styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,6 @@
-import image from "../../public/assets/images/image.jpg";
 import Typed from "typed.js";
 import { useEffect, useRef, useState } from "react";
-import BlurWrapper from "../components/helpers/BlurWarpper";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import TextTransition, { presets } from "react-text-transition";
 import TitleAdder from "../HOC/TitleAdder";
 
@@ -29,30 +27,48 @@ const Home = () => {
   }, []);
 
   return (
-    <BlurWrapper imageUrl={image}>
-      <Typography
-        ref={nameEl}
-        variant="h3"
-        color="primary.main"
-        sx={{ textAlign: "center", p: 2 }}
-      />
-      <TextTransition springConfig={presets.gentle}>
-        {showText && (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background:
+          "linear-gradient(135deg, #ff7e5f, #feb47b, #86a8e7, #91eae4)",
+        backgroundSize: "400% 400%",
+        animation: "gradientAnimation 15s ease infinite",
+        backdropFilter: "blur(10px)",
+        zIndex: 1,
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          ref={nameEl}
+          variant="h3"
+          color="primary.main"
+          sx={{ p: 2 }}
+        />
+        <TextTransition springConfig={presets.gentle}>
           <Typography
             variant="h4"
             color="whitesmoke"
             sx={{
-              textAlign: "center",
               p: 2,
               textDecoration: "underline",
               textDecorationColor: "#7A5DCB",
             }}
           >
-            توسعه دهنده فرانت اند (ReactJs)
+            {showText ? "توسعه دهنده فرانت اند (ReactJs)" : ""}
           </Typography>
-        )}
-      </TextTransition>
-    </BlurWrapper>
+        </TextTransition>
+      </Box>
+    </Box>
   );
 };
 
